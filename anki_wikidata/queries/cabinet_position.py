f842cbef2775ed61bfc9336771915aee12b8019d
Langston Barrett <langston.barrett@gmail.com>
2022-12-29 16:13:04 -0500

    Two more queries

--- anki_wikidata/queries/cabinet_position.py
+++ anki_wikidata/queries/cabinet_position.py
@@ -5,25 +5,29 @@ from queries.util import get_results
 from ..card import Card
 
 QUERY = """
-SELECT DISTINCT ?name ?date WHERE {
+SELECT DISTINCT ?name ?nPos WHERE {
   SERVICE wikibase:label { bd:serviceParam wikibase:language "en_US". }
 
-  {ITEM} wdt:P585 ?date .
+  {ITEM} wdt:P39 ?pos .
+  ?pos wdt:P31 wd:Q639738 .
+  ?pos rdfs:label ?nPos .
+  filter(lang(?nPos) = "en") .
 
   {ITEM} rdfs:label ?name .
   filter(lang(?name) = "en") .
-} 
+}
 """
 
 
 def query(entity: str) -> list[Card]:
+    assert "{ITEM}" in QUERY
     results = get_results(QUERY.replace("{ITEM}", entity))
     if len(results) != 1:
         return []
     return [
         Card(
-            id=f"happened-year-{entity[3:]}",
-            front=f"In what year did the {results[0]['name']} happen?",
-            back=results[0]["date"][:4],
+            id=f"part-of-war-{entity[3:]}",
+            front=f"What cabinet position did {results[0]['name']} hold?",
+            back=results[0]["nPos"],
         )
     ]
--- anki_wikidata/queries/part_of_war.py
+++ anki_wikidata/queries/part_of_war.py
@@ -4,26 +4,31 @@ from queries.util import get_results
 
 from ..card import Card
 
+# TODO: Allow subclass of war
 QUERY = """
-SELECT DISTINCT ?name ?date WHERE {
+SELECT DISTINCT ?name ?nWar WHERE {
   SERVICE wikibase:label { bd:serviceParam wikibase:language "en_US". }
 
-  {ITEM} wdt:P585 ?date .
+  {ITEM} wdt:P361 ?war .
+  ?war wdt:P31 wd:Q198 .
+  ?war rdfs:label ?nWar .
+  filter(lang(?nWar) = "en") .
 
   {ITEM} rdfs:label ?name .
   filter(lang(?name) = "en") .
-} 
+}
 """
 
 
 def query(entity: str) -> list[Card]:
+    assert "{ITEM}" in QUERY
     results = get_results(QUERY.replace("{ITEM}", entity))
     if len(results) != 1:
         return []
     return [
         Card(
-            id=f"happened-year-{entity[3:]}",
-            front=f"In what year did the {results[0]['name']} happen?",
-            back=results[0]["date"][:4],
+            id=f"part-of-war-{entity[3:]}",
+            front=f"What war was the {results[0]['name']} part of?",
+            back=results[0]["nWar"],
         )
     ]
